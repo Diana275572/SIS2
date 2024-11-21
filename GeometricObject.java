91860a96eb806e1557febc1736c21c956350fb0a
Diana275572 <diana2.005@icloud.com>
2024-11-21 09:36:04 +0500

    Update GeometricObject.java

--- GeometricObject.java
+++ GeometricObject.java
@@ -4,29 +4,5 @@ abstract class GeometricObject implements Serializable {
     abstract double area();
 }
 
-class Circle extends GeometricObject {
-    private double radius;
 
-    public Circle(double radius) {
-        this.radius = radius;
-    }
 
-    @Override
-    double area() {
-        return Math.PI * radius * radius;
-    }
-}
-
-class Rectangle extends GeometricObject {
-    private double length, width;
-
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    @Override
-    double area() {
-        return length * width;
-    }
-}

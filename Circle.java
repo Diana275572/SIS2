d9fcb8b0574ec34d25f0f6d6cbd0a26e6eb2e272
Diana275572 <diana2.005@icloud.com>
2024-11-21 09:38:30 +0500

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,13 @@
+class Circle extends GeometricObject {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
